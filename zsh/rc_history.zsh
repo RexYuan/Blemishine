9ee55c70b3ab33bb511fbe0bc2cd945a1ca6fa44
RexYuan <r1218r1218@gmail.com>
2021-06-04 22:38:46 +0800

    Added zsh history options

--- zsh/rc_history.zsh
+++ zsh/rc_history.zsh
@@ -5,3 +5,8 @@ HISTFILE_DIR="$HOME/.cache/zsh"
 HISTFILE="$HISTFILE_DIR/.zsh_history"
 HISTSIZE=3000
 SAVEHIST=3000
+
+setopt INC_APPEND_HISTORY # immediate append
+setopt EXTENDED_HISTORY # timestamp
+setopt HIST_IGNORE_DUPS # ignore consecutive dups
+setopt HIST_FIND_NO_DUPS # don't show dups in search
