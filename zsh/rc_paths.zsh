4dfeba1ab6457cca295ee5a11382b6df5078840c
RexYuan <r1218r1218@gmail.com>
2022-09-27 01:45:00 +0800

    Updated stuff

--- zsh/rc_paths.zsh
+++ zsh/rc_paths.zsh
@@ -1,5 +1,15 @@
 
 export PATH="/usr/local/sbin:$PATH"
-export PATH="/Users/rexyuan/Library/Python/3.8/bin:$PATH"
 
-export LDFLAGS="-L/usr/local/opt/python@3.8/lib"
+export PATH="/Users/rexyuan/Library/Python/3.9:/Users/rexyuan/Library/Python/3.8/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/python@3.9/lib -L/usr/local/opt/python@3.8/lib"
+# pipx
+export PATH="/Users/rexyuan/.local/bin:$PATH"
+
+# Scala
+# >>> JVM installed by coursier >>>
+export JAVA_HOME="/Users/rexyuan/Library/Caches/Coursier/arc/https/github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_mac_hotspot_8u292b10.tar.gz/jdk8u292-b10/Contents/Home"
+# <<< JVM installed by coursier <<<
+# >>> coursier install directory >>>
+export PATH="$PATH:/Users/rexyuan/Library/Application Support/Coursier/bin"
+# <<< coursier install directory <<<
