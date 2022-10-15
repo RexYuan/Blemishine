4f36ac456881cb3e7d58ff8204e344c68a68a35c
RexYuan <r1218r1218@gmail.com>
2022-10-16 01:51:38 +0800

    Enable z jump

--- zsh/rc_libs.zsh
+++ zsh/rc_libs.zsh
@@ -5,5 +5,8 @@ PURE_PROMPT_SYMBOL="🌸  ❯"
 autoload -U promptinit; promptinit
 prompt pure
 
+# z jump
+source /usr/local/etc/profile.d/z.sh
+
 # must be sourced last
 source "$BLEM/zsh/lib/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
