343e81e24a3ed75c9fee0a8a42421f7c6e3b9072
RexYuan <r1218r1218@gmail.com>
2022-10-17 15:25:32 +0800

    Added bookmarklets

---
+++ bookmarklet/h.js
@@ -0,0 +1,1 @@
+javascript:var style=document.createElement('style');style.innerText='body{max-width: 50%;margin: auto;}';var head=document.getElementsByTagName('head')[0];head.appendChild(style);
---
+++ bookmarklet/hscp.js
@@ -0,0 +1,1 @@
+javascript: document.body.style.maxWidth="50%"; document.body.style.margin="auto";document.body.style.display="-webkit-box"; null;
