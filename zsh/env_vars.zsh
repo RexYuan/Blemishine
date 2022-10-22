b747f31027037eb227f05b6a8dd65dc62c5519c6
RexYuan <r1218r1218@gmail.com>
2022-10-22 23:58:00 +0800

    Changed editor option

--- zsh/env_vars.zsh
+++ zsh/env_vars.zsh
@@ -5,9 +5,21 @@ export SHELL_SESSIONS_DISABLE=1
 # https://stackoverflow.com/q/65259300/2448540
 arch="$(uname -m)"
 if [[ "$arch" == 'arm64' ]]; then
-    BREW_PREFIX="/opt/homebrew"
+    export BREW_PREFIX="/opt/homebrew"
 elif [[ "$arch" == 'x86_64' ]]; then
-    BREW_PREFIX="/usr/local"
+    export BREW_PREFIX="/usr/local"
 else
     exit 1
 fi
+
+# https://askubuntu.com/q/1021554/444384
+if [[ "$TERM_PROGRAM" == "vscode" ]]; then
+    export EDITOR="code --wait --new-window"
+    export GIT_EDITOR="code --wait --new-window"
+elif [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]; then
+    export EDITOR="nano"
+    export GIT_EDITOR="nano"
+else
+    exit 1
+fi
+
