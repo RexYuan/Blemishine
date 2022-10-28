c61b66d06b5ca67eb45551264bfc88df0c24fdd9
RexYuan <r1218r1218@gmail.com>
2022-10-29 02:45:25 +0800

    Set up env vars for git

--- zsh/env_vars.zsh
+++ zsh/env_vars.zsh
@@ -14,23 +14,15 @@ fi
 
 # https://askubuntu.com/q/1021554/444384
 if [[ "$TERM_PROGRAM" == "vscode" ]]; then
-    export PAGER="code"
-    export GIT_PAGER="code --wait --reuse-window"
-    export EDITOR="code"
-    export GIT_EDITOR="code --wait --reuse-window"
-    export VISUAL="code"
+    export EDITOR="code --wait"
+    export VISUAL="code --wait"
 elif [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]; then
-    export PAGER="less"
-    export GIT_PAGER="less"
     export EDITOR="nano"
-    export GIT_EDITOR="nano"
     export VISUAL="nano"
 else
-    echo "⚠️  PAGER not set"
-    echo "⚠️  GIT_PAGER not set"
     echo "⚠️  EDITOR not set"
-    echo "⚠️  GIT_EDITOR not set"
     echo "⚠️  VISUAL not set"
 fi
 
+export PAGER="less"
 export BROWSER="open"
