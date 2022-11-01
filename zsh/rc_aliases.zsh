2898ff6946fd98274dc33078a7bd9aca598f9740
RexYuan <r1218r1218@gmail.com>
2022-11-02 00:27:23 +0800

    Renamed some zsh functions file names

--- zsh/rc_aliases.zsh
+++ zsh/rc_aliases.zsh
@@ -3,4 +3,5 @@ alias uvm-on="vboxmanage startvm ubuntu20 --type headless"
 alias uvm-off="vboxmanage controlvm ubuntu20 poweroff"
 alias uvm-info="vboxmanage showvminfo ubuntu20"
 
+# https://superuser.com/q/183876/372113
 alias ls="ls -G"
