ed899d89f62c5c4f7c2e0021ca9e0975f87f1ad0
RexYuan <r1218r1218@gmail.com>
2022-11-02 00:26:18 +0800

    Added color to ls

--- zsh/rc_aliases.zsh
+++ zsh/rc_aliases.zsh
@@ -2,3 +2,5 @@
 alias uvm-on="vboxmanage startvm ubuntu20 --type headless"
 alias uvm-off="vboxmanage controlvm ubuntu20 poweroff"
 alias uvm-info="vboxmanage showvminfo ubuntu20"
+
+alias ls="ls -G"
