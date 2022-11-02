f1855617417613aebc500b007a1c7badb112e83b
RexYuan <r1218r1218@gmail.com>
2022-11-03 00:48:20 +0800

    Fixed Makefile and added guard for zsh

--- zsh/rc_funcs.zsh
+++ zsh/rc_funcs.zsh
@@ -1,6 +1,11 @@
 
 function mapargs
 {
+    if [[ $# -lt 2 ]]; then
+        echo $#
+        echo "usage: mapargs <function> <arg1>..."
+        return 1
+    fi
     for arg in ${@:2}
     do
         $1 $arg
