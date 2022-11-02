3082535271d5edeb8f63d53d84cd7f5d32cc6b6f
RexYuan <r1218r1218@gmail.com>
2022-11-03 01:50:54 +0800

    Added fd options and fixed color options

--- zsh/rc_aliases.zsh
+++ zsh/rc_aliases.zsh
@@ -4,10 +4,11 @@ alias uvm-off="vboxmanage controlvm ubuntu20 poweroff"
 alias uvm-info="vboxmanage showvminfo ubuntu20"
 
 # https://superuser.com/q/183876/372113
-alias ls="ls -G"
+alias ls="ls --color=auto"
 # https://stackoverflow.com/q/2327191/2448540
-alias grep="grep --color=always"
+alias grep="grep --color=auto"
 # https://stackoverflow.com/q/7851134/2448540
 alias cat="bat --theme Tomorrow-Night --paging=never"
 # https://superuser.com/q/117841/372113
 alias less="bat --theme Tomorrow-Night --paging=always"
+alias fd="fd --hidden --no-ignore --follow --color=auto"
