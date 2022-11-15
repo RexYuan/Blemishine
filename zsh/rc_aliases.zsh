39878e61b68bd3c240e6b960e4914bf17e9b2749
RexYuan <r1218r1218@gmail.com>
2022-11-16 03:53:41 +0800

    Keybinds and gits

--- zsh/rc_aliases.zsh
+++ zsh/rc_aliases.zsh
@@ -5,11 +5,17 @@ alias uvm-info="vboxmanage showvminfo ubuntu20"
 
 # https://superuser.com/q/183876/372113
 alias ls="ls --color=auto"
+
 # https://stackoverflow.com/q/2327191/2448540
 alias grep="grep --color=auto"
-# https://stackoverflow.com/q/7851134/2448540
-alias cat="bat --theme Tomorrow-Night --paging=never"
-# https://superuser.com/q/117841/372113
-alias less="bat --theme Tomorrow-Night --paging=always"
-alias fd="fd --hidden --no-ignore --follow --color=auto"
-alias ag="ag --pager \"bat --theme Tomorrow-Night --paging=always\" --unrestricted"
+
+if command -v bat &> /dev/null; then \
+	# https://stackoverflow.com/q/7851134/2448540
+    alias cat="bat --theme Tomorrow-Night --paging=never"
+
+    # https://superuser.com/q/117841/372113
+    alias less="bat --theme Tomorrow-Night --paging=always"
+
+    alias fd="fd --hidden --no-ignore --follow --color=auto"
+    alias ag="ag --pager \"bat --theme Tomorrow-Night --paging=always\" --unrestricted"
+fi
