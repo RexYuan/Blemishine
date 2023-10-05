c69dcfd7703b77470d96f941cc97e9729ef49a63
RexYuan <r1218r1218@gmail.com>
2023-10-06 00:14:02 +0800

    No need for login

--- zsh/functions/gallery-dl.zsh
+++ zsh/functions/gallery-dl.zsh
@@ -7,12 +7,12 @@
 # gallery-dl --download-archive archive.sqlite3 -o skip=true https://www.instagram.com/neo.omochi/
 
 # twitter: continue download, skipping downloaded according to archive
-# gallery-dl --download-archive archive.sqlite3 -o skip=true -u username -p password --cookies cookies.txt https://twitter.com/tkgw1500cc/media
-# gallery-dl --download-archive archive.sqlite3 -o skip=true -u username -p password --cookies cookies.txt https://twitter.com/tkgw02/media
+# gallery-dl --download-archive archive.sqlite3 -o skip=true --cookies cookies.txt https://twitter.com/tkgw1500cc/media
+# gallery-dl --download-archive archive.sqlite3 -o skip=true --cookies cookies.txt https://twitter.com/tkgw02/media
 
 ###
-# gallery-dl --download-archive archive.sqlite3 -o skip=true -u username -p password --cookies cookies.txt https://twitter.com/Glico_happy/media
+# gallery-dl --download-archive archive.sqlite3 -o skip=true --cookies cookies.txt https://twitter.com/Glico_happy/media
 
 ###
 # gallery-dl --download-archive archive.sqlite3 -o skip=true https://www.instagram.com/banbanko_/
-# gallery-dl --download-archive archive.sqlite3 -o skip=true -u username -p password --cookies cookies.txt https://twitter.com/Banbanko_/media
+# gallery-dl --download-archive archive.sqlite3 -o skip=true --cookies cookies.txt https://twitter.com/Banbanko_/media
