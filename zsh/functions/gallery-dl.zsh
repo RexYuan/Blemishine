504cb05812d4c5e73ee201f8e954a9e5abaa2131
RexYuan <r1218r1218@gmail.com>
2023-10-06 00:05:36 +0800

    Added stuff

---
+++ zsh/functions/gallery-dl.zsh
@@ -0,0 +1,18 @@
+
+
+# use this to fill up archive from everything online
+# gallery-dl --download-archive archive.sqlite3 --no-download -o skip=false https://www.instagram.com/neo.omochi/
+
+# instagram: continue download, skipping downloaded according to archive
+# gallery-dl --download-archive archive.sqlite3 -o skip=true https://www.instagram.com/neo.omochi/
+
+# twitter: continue download, skipping downloaded according to archive
+# gallery-dl --download-archive archive.sqlite3 -o skip=true -u username -p password --cookies cookies.txt https://twitter.com/tkgw1500cc/media
+# gallery-dl --download-archive archive.sqlite3 -o skip=true -u username -p password --cookies cookies.txt https://twitter.com/tkgw02/media
+
+###
+# gallery-dl --download-archive archive.sqlite3 -o skip=true -u username -p password --cookies cookies.txt https://twitter.com/Glico_happy/media
+
+###
+# gallery-dl --download-archive archive.sqlite3 -o skip=true https://www.instagram.com/banbanko_/
+# gallery-dl --download-archive archive.sqlite3 -o skip=true -u username -p password --cookies cookies.txt https://twitter.com/Banbanko_/media
