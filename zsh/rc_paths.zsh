755c0c4b16e23b999a80cd52632ad784665dd1a2
RexYuan <r1218r1218@gmail.com>
2024-04-06 21:52:08 +0800

    added conda path

--- zsh/rc_paths.zsh
+++ zsh/rc_paths.zsh
@@ -47,3 +47,18 @@ export PATH="/usr/local/opt/bison/bin:$PATH"
 
 # curl
 export PATH="/usr/local/opt/curl/bin:$PATH"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/usr/local/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/usr/local/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/usr/local/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/usr/local/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
