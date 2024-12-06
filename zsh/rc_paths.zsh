48faec014bf8227e0207c67ae981d6be561e2799
RexYuan <r1218r1218@gmail.com>
2024-12-07 01:33:01 +0800

    pkgs

--- zsh/rc_paths.zsh
+++ zsh/rc_paths.zsh
@@ -2,18 +2,6 @@
 # Set PATH, MANPATH, etc., for Homebrew.
 eval "$($BREW_PREFIX/bin/brew shellenv)"
 
-# Scala
-# >>> JVM installed by coursier >>>
-export JAVA_HOME="/Users/rexyuan/Library/Caches/Coursier/arc/https/github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_mac_hotspot_8u292b10.tar.gz/jdk8u292-b10/Contents/Home"
-# <<< JVM installed by coursier <<<
-# >>> coursier install directory >>>
-if [[ -d "$HOME/Library/Application Support/Coursier" ]]; then
-    export PATH="$PATH:$HOME/Library/Application Support/Coursier/bin"
-else
-    echo "🟠 Coursier not set up. Run \"cs setup\""
-fi
-# <<< coursier install directory <<<
-
 # rustup
 if [[ -f "$HOME/.cargo/env" ]]; then
     source "$HOME/.cargo/env"
@@ -62,3 +50,10 @@ else
 fi
 unset __conda_setup
 # <<< conda initialize <<<
+
+# java
+export JAVA_HOME="/usr/local/opt/openjdk@11"
+
+# >>> coursier install directory >>>
+export PATH="$PATH:/Users/rexyuan/Library/Application Support/Coursier/bin"
+# <<< coursier install directory <<<
